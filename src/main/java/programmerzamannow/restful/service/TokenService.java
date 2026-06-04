98be491ed7c5fc7e56b9063c0d7da399005749b0
zenfikry <zenfikry@gmail.com>
2026-06-04 11:21:18 +0700

    implement redis

--- src/main/java/programmerzamannow/restful/service/TokenService.java
+++ src/main/java/programmerzamannow/restful/service/TokenService.java
@@ -4,7 +4,11 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import programmerzamannow.restful.entity.User;
 
 import java.time.Duration;
@@ -17,15 +21,27 @@ public class TokenService {
 
     private final JWTVerifier verifier = JWT.require(algorithm).build();
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     public String create(User user) {
-        return JWT.create()
+        String token = JWT.create()
                 .withClaim("name", user.getName())
                 .withClaim("username", user.getUsername())
                 .withExpiresAt(Instant.now().plus(Duration.ofDays(30)))
                 .sign(algorithm);
+
+        /* save token on redis */
+        redisTemplate.opsForValue().set(token, user.getUsername(), Duration.ofDays(30));
+
+        return token;
     }
 
     public User fromToken(String token) {
+        if (!redisTemplate.hasKey(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token expired");
+        }
+
         DecodedJWT jwt = verifier.verify(token);
 
         User user = new User();
